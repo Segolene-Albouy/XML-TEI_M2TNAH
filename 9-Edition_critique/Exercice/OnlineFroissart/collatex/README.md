15340e8466dff8434ab4b9473283bb925bc91353
Segolene <segolene.albouy@gmail.com>
2022-01-03 14:22:07 +0100

    Session 9: add slides and exercices

---
+++ 9-Edition_critique/Exercice/OnlineFroissart/collatex/README.md
@@ -0,0 +1,18 @@
+# Utiliser CollateX
+
+- Création d'un environnement virtuel
+``` bash
+python3 -m venv venv
+```
+- Activatuon de l'environnement
+``` bash
+source venv/bin/activate
+```
+- Installation des dépendances
+``` bash
+python3 -m pip install -r requirements.txt
+```
+- Lancement du script
+``` bash
+python3 collation.py
+```
---
+++ 9-Edition_critique/Exercice/OnlineFroissart/collatex/collation.py
@@ -0,0 +1,18 @@
+from collatex import *
+
+with open('../Austin-48.txt') as f:
+    austin = " ".join(f.readlines())
+with open('../Paris-fr-2663.txt') as f:
+    paris = " ".join(f.readlines())
+with open('../Stonyhurst-1.txt') as f:
+    stonyhurst = " ".join(f.readlines())
+
+collation = Collation()
+collation.add_plain_witness("A", austin)
+collation.add_plain_witness("P", paris)
+collation.add_plain_witness("S", stonyhurst)
+
+alignment_table = collate(collation, output="tei", segmentation=False, near_match=True)
+
+with open("collation.xml", "w") as output:
+	output.write(alignment_table)
